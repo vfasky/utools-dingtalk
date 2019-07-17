322d6486ab942d0b4d7c34cf45d2501b9c0045d8
vfasky <vfasky@gmail.com>
2019-07-17 11:04:47 +0800

    style 调整代码结构

--- preload.js
+++ preload.js
@@ -1,30 +1,21 @@
+const fs = require('fs')
+const path = require('path')
 /**
- * 
+ * 插入样式
  * @param {Document} doc
  */
 function addStyle(doc) {
     let style = document.createElement('style')
-    let css = `
-body .login-form .tab-items {
-    display: none;
-}
-body #layout-main {
-    width: 100%;
-    height: 100%;
-    flex: none;
-    border: none;
-    box-shadow: none;
-}
-body #layout-main #body{
-    height: 100%;
-}
-`
+    let css = fs.readFileSync(
+        path.join(__dirname, 'style.css'),
+        'utf8'
+    )
     style.innerHTML = css
     doc.head.appendChild(style)
 }
 
 /**
- * 
+ * dingtalk iframe 加载完执行
  * @param {HTMLIFrameElement} main 
  */
 function dingTalkOnload (main) {
@@ -33,11 +24,24 @@ function dingTalkOnload (main) {
     addStyle(mainDoc)
 }
 
-utools.onPluginReady(async () => {
+/**
+ * 初始化 web dingtalk
+ */
+function dingTalkInit() {
     const iframe = document.createElement('iframe')
     iframe.onload = () =>　dingTalkOnload(iframe)
     iframe.src = 'https://im.dingtalk.com/'
     iframe.setAttribute('scrolling', 'no')
     iframe.setAttribute('frameborder', '0')
     document.body.appendChild(iframe)
+}
+
+// 插件初始化完成
+utools.onPluginReady(() => {
+    dingTalkInit()
+})
+
+// 插件被分离
+utools.onPluginDetach(() => {
+
 })
